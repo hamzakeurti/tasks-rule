ae0215607d2daa13a8ede58cbf27e2e7060134e7
ValentinYiu <valentinY@protonmail.com>
2019-12-04 13:46:28 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,4 +4,26 @@ import torch.nn as nn
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder,self).__init__()
-        pass
+        self.conv1 = nn.Conv2d(3,64,7,padding = 7//2,stride = 2)
+        self.conv2 = nn.Conv2d(64,128,3,padding = 3//2)
+        self.conv3 = nn.Conv2d(128,256,3,padding = 3//2)
+        self.conv4 = nn.Conv2d(256,512,3,padding = 3//2)
+
+        self.pool = nn.MaxPool2d(3,padding = 3//2,stride = 2)
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        x = self.pool(self.relu(self.conv1(x)))
+        V1 = x
+
+        x = self.pool(self.relu(self.conv2(x)))
+        V2 = x
+
+        x = self.pool(self.relu(self.conv3(x)))
+        V4 = x
+
+        x = self.pool(self.relu(self.conv4(x)))
+        IT = x
+
+        return V1,V2,V4,IT
+        
