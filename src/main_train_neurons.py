abc1e979048dc42c3ea7a341eaecf214ff1e45be
ValentinYiu <valentinY@protonmail.com>
2019-12-22 11:54:12 +0800

    Update main_train_neurons.py

--- src/main_train_neurons.py
+++ src/main_train_neurons.py
@@ -102,9 +102,4 @@ if __name__=='__main__':
         if test_loss<min_loss:
             min_loss = test_loss
             best_model = deepcopy(reg_model)
-    prefix = args.task_name
-    np.save(prefix + '_test_losses.npy',test_losses)
-    np.save(prefix + '_train_losses.npy',train_losses)
-    np.save(prefix + '_test_accs.npy',test_accs)
-    np.save(prefix + '_train_accs.npy',train_accs)
     torch.save(best_model.state_dict(),"saved_models/"+args.task_name+"_reg_model.pth")
