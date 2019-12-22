b8d10ad1e741c048582d846f767b4ddf740a5912
Haoran Chen <chenhaoran.earth@live.com>
2019-12-22 08:04:10 +0800

    add support for multi-label classification

--- src/main_train_task.py
+++ src/main_train_task.py
@@ -9,11 +9,13 @@ from model import Encoder,Classification_Decoder
 from copy import deepcopy
 from dataset import Classification_dataset
 import argparse
+from sklearn.metrics import average_precision_score
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--task_name",type=str,help="Name of the task for file naming purposes")
 parser.add_argument("--task_index",type=int,help="Index of the task")
-parser.add_argument("--dataset",type=str,help = "pickle file associated to the dataset",default = "data/single-class-classification-dataset.pkl")
+parser.add_argument("--dataset",type=str,help="pickle file associated to the dataset",default="data/single-class-classification-dataset.pkl")
 
 parser.add_argument("--device",type = str,default="cuda")
 
@@ -22,13 +24,11 @@ parser.add_argument("--LOG_INTERVAL",type=int,default=50)
 parser.add_argument("--EPOCHS",type=int,default=50)
 
 
-parser.add_argument("--momentum",type = float,default=0.)
 parser.add_argument("--learning_rate",type=float,default=0.01)
 parser.add_argument("--weight_decay",type=float,default=0.)
 parser.add_argument("--Encoder_out",type=int,default=512)
 parser.add_argument("--Decoder_out",type=int,default=10)
 
-parser.add_argument("--multitask_mode",type=int,default=0)
 
 args = parser.parse_args()
 
@@ -46,43 +46,62 @@ encoder = Encoder().to(device)
 decoder = Classification_Decoder(args.Encoder_out,args.Decoder_out).to(device)
 
 params = list(encoder.parameters()) + list(decoder.parameters())
-optimizer = optim.SGD(params, lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
+optimizer = optim.Adam(params, lr=args.learning_rate, weight_decay=args.weight_decay, amsgrad=True)
 
 def train(encoder,decoder, device, train_loader, optimizer, epoch):
     encoder.train()
     decoder.train()
     loss_list = []
     acc_list = []
+    outputs, targets = [], []
     saved_loss_list = []
     saved_acc_list = []
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), torch.tensor(target).to(device)
-        if args.multitask_mode==1:
+        if args.task_index == 3:
                 target = target.type(torch.float)
         optimizer.zero_grad()
         output = encoder(data)
         output = output[4].view(-1,args.Encoder_out)
         output = decoder(output)
-        if args.multitask_mode ==0:
+        if args.task_index in [1, 2, 4, 5]:
             loss = F.cross_entropy(output, target)
             pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
             acc = pred.eq(target.view_as(pred)).float().mean()
+            acc_list.append(acc.item())
         else:
             loss = F.binary_cross_entropy_with_logits(output,target)
-            acc=torch.tensor([0])
+            outputs.append(output.cpu().numpy())
+            targets.append(target.cpu().numpy())
         loss.backward()
         optimizer.step()
         loss_list.append(loss.item())
-        acc_list.append(acc.item())
+
         if batch_idx % args.LOG_INTERVAL == 0:
-            msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tAvg Loss: {:.4f}\tAvg Acc: {:.4f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), np.mean(loss_list), np.mean(acc_list))
-            LOG_INFO(msg)
-            saved_loss_list.append(np.mean(loss_list))
-            saved_acc_list.append(np.mean(acc_list))
-            loss_list.clear()
-            acc_list.clear()
+            if args.task_index in [1, 2, 4, 5]:
+                msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tAvg Loss: {:.4f}\tAvg Acc: {:.4f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), np.mean(loss_list), np.mean(acc_list))
+                LOG_INFO(msg)
+                saved_loss_list.append(np.mean(loss_list))
+                saved_acc_list.append(np.mean(acc_list))
+                loss_list.clear()
+                acc_list.clear()
+            else:
+                y_true = np.concatenate(targets, axis=0)
+                y_score = np.concatenate(outputs, axis=0)
+                average_precision = average_precision_score(y_true, y_score)
+                saved_loss_list.append(np.mean(loss_list))
+                saved_acc_list.append(average_precision)
+                msg = 'Train Epoch: {} [{}/{} ({:.0f}%)]\tAvg Loss: {:.4f}\tAvg Precision: {:.4f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), saved_loss_list[-1], average_precision)
+                LOG_INFO(msg)
+
+                loss_list.clear()
+                targets.clear()
+                outputs.clear()
+
     return saved_loss_list,saved_acc_list
 
 
